ce37c4b1aab8378860f20b8d217b5bd1ae431e8a
nola4211 <fordnola@byu.edu>
2026-02-09 14:26:53 -0700

    Delete src directory

--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.accordion-header').forEach(btn => {
-    btn.addEventListener('click', () => {
-        const item = btn.parentElement;
-        item.classList.toggle('active');
-    });
-});
